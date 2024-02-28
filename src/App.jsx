33547d0b483a75b1e8f8f5527a40effcc9d7705c
Julian Finelli <julianfinelli97@gmail.com>
2024-02-28 14:57:02 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Principal from './components/Principal/Principal'
+
+function App() {
+
+  return (
+    <>
+      <Principal/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Principal/Principal.jsx
@@ -0,0 +1,9 @@
+import "./principal.css"
+
+export default function Principal() {
+  return (
+    <div className="contenedorPrincipal">
+        Principal
+    </div>
+  )
+}
